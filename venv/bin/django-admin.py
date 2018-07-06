53404a53e4b80eb5fe4caa2181b48f62d5494dbc
Abhiram Natarajan <yellowantapps@gmail.com>
2018-07-06 12:12:19 +0530

    Push

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/15afd8d4-23a1-4733-9204-2964d3811cd6/yellowant/yellowant-agile-CRM/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
